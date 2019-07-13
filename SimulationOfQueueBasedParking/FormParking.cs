ff4bfa6785186d9594ee32cb52305095e17d6b3a
Buse <buse@Buse-MacBook-Pro.local>
2019-07-13 13:58:32 +0300

    Simulation Of Queue Based Parking

---
+++ SimulationOfQueueBasedParking/FormParking.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulationOfQueueBasedParking
+{
+    public partial class FormParking : Form
+    {
+        public FormParking()
+        {
+            InitializeComponent();
+        }
+
+        public static int carNumber;
+        public static int parkingStatus; // 1 indicates that the car park is full, 0 indicates that the car park is empty.
+
+        private void FormParking_Load(object sender, EventArgs e)
+        {
+            btnListele.Visible = false;
+            btnOncelikliListele.Visible = false;
+            label2.Visible = false;
+            checkBoxKazanc.Visible = false;
+            checkBoxSil.Visible = false;
+            btnSil.Visible = false;
+            btnOncelikliSil.Visible = false;
+            btnKazancHesapla.Visible = false;
+        }
+
+        private void BtnOtoparkArabaEkle_Click(object sender, EventArgs e)
+        {
+            btnListele.Visible = true;
+            btnOncelikliListele.Visible = true;
+            label2.Visible = true;
+            checkBoxKazanc.Visible = true;
+            checkBoxSil.Visible = true;
+
+            carNumber = Convert.ToInt32(txtArabaSayisi.Text);
+            Parking parking = new Parking();
+            parking.AddCarToParking();
+            parking.AddToPriorityCar();
+
+            btnOtoparkArabaEkle.Enabled = false;
+            MessageBox.Show("The cars added to park lot.");
+            parkingStatus = 1;
+        }
+
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            carNumber = Convert.ToInt32(txtArabaSayisi.Text);
+            Parking parking = new Parking();
+            MessageBox.Show(parking.ListCarParking() + "Average Processing Time : " + parking.IslemSuresiOrtalamaHesapla());
+        }
+
+        private void BtnOncelikliListele_Click(object sender, EventArgs e)
+        {
+            carNumber = Convert.ToInt32(txtArabaSayisi.Text);
+            Parking parking = new Parking();
+            MessageBox.Show(parking.ListPriorityCar() + "Average Processing Time : " + parking.IslemSuresiOrtalamaHesapla());
+        }
+
+        private void CheckBoxKazanc_CheckedChanged(object sender, EventArgs e)
+        {
+            if (parkingStatus == 1)
+            {
+                if (checkBoxKazanc.Checked == true)
+                {
+                    checkBoxSil.Enabled = false;
+                    btnKazancHesapla.Visible = true;
+                }
+                else
+                {
+                    checkBoxSil.Enabled = true;
+                    btnKazancHesapla.Visible = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Parking is empty. Please, try again when parking is full.");
+            }
+        }
+
+        private void CheckBoxSil_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxSil.Checked == true)
+            {
+                checkBoxKazanc.Enabled = false;
+                btnSil.Visible = true;
+                btnOncelikliSil.Visible = true;
+            }
+            else
+            {
+                checkBoxKazanc.Enabled = true;
+                btnSil.Visible = false;
+                btnOncelikliSil.Visible = false;
+            }
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            carNumber = Convert.ToInt32(txtArabaSayisi.Text);
+            Parking parking = new Parking();
+            MessageBox.Show(parking.RemoveCarParking());
+
+            parkingStatus = 0;
+        }
+
+        private void BtnOncelikliSil_Click(object sender, EventArgs e)
+        {
+            carNumber = Convert.ToInt32(txtArabaSayisi.Text);
+            Parking parking = new Parking();
+            MessageBox.Show(parking.OncelikliArabaSil());
+
+            parkingStatus = 0;
+        }
+
+        private void BtnKazancHesapla_Click(object sender, EventArgs e)
+        {
+            Parking parking = new Parking();
+            MessageBox.Show(parking.CalculateEarnings());
+        }
+    }
+}
---
+++ SimulationOfQueueBasedParking/Parking.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationOfQueueBasedParking
+{
+    class Parking
+    {
+        static int carNumber = FormParking.carNumber;
+        public static SimpleQueue simpleQueue = new SimpleQueue(carNumber);
+        public static PriorityQueue priorityQueue = new PriorityQueue(carNumber);
+
+        public void AddCarToParking()
+        {
+            Random randomProcessingTime = new Random();
+
+            for (int i = 0; i < carNumber; i++)
+            {
+                Car a = new Car();
+                a.ProcessingTime = randomProcessingTime.Next(10, 300);
+                a.Number = i + 1;
+                simpleQueue.Insert(a);
+            }
+        }
+
+        public string ListCarParking()
+        {
+            string strList = "";
+            foreach (Car a in simpleQueue.Peek())
+            {
+                strList += "" + a.Number + " Processing Time Of The Numbered Vehicle : " + a.ProcessingTime + Environment.NewLine;
+            }
+            return strList;
+        }
+
+        public string RemoveCarParking()
+        {
+            int passingTime = 0, i = 1;
+            string strList = "";
+            foreach (Car a in simpleQueue.Peek())
+            {
+                passingTime += a.ProcessingTime;
+                strList += i++ + "nd car from exit the parking lot --> " + "Stay in the queue time of the number " + a.Number + " vehicle"+ passingTime + Environment.NewLine;
+                simpleQueue.Remove();
+            }
+            return strList;
+        }
+
+        public void AddToPriorityCar()
+        {
+            Car b = new Car();
+            foreach (Car a in simpleQueue.Peek())
+            {
+                b = a;
+                priorityQueue.Insert(b);
+            }
+        }
+
+        public string ListPriorityCar()
+        {
+            string strList = "";
+            foreach (Car a in priorityQueue.Peek())
+            {
+                strList += "" +  "Processing time of vehicle number : " + a.Number + a.ProcessingTime + Environment.NewLine;
+            }
+            return strList;
+        }
+
+        public string PriorityCarDelete()
+        {
+            int passingTime = 0, i = 1;
+            string strList = "";
+            foreach (Car a in priorityQueue.Peek())
+            {
+                passingTime += a.ProcessingTime;
+                strList += i++ + "nd car from exit the parking lot --> " + "Stay in the queue time of the number : "  + a.Number + passingTime + Environment.NewLine;
+            }
+            foreach (Car b in priorityQueue.Peek())
+            {
+                priorityQueue.Remove();
+            }
+            return strList;
+        }
+
+        public int AverageProcessingTimeCalculation()
+        {
+            int total = 0, avg = 0;
+            foreach (Car a in simpleQueue.Peek())
+            {
+                total += a.ProcessingTime;
+            }
+            avg = total / carNumber;
+            return avg;
+        }
+
+        public string CalculateEarnings()
+        {
+            double[,] array = new double[carNumber, 3];
+            int passingTime = 0, i = 0, carCounter = 1;
+            double difference = 0, percentage = 0;
+            string strDifferencePercentage = "";
+
+            foreach (Car a in simpleQueue.Peek())
+            {
+                passingTime += a.ProcessingTime;
+                array[i, 0] = carCounter;
+                array[i, 1] = passingTime;
+                i++;
+                carCounter++;
+            }
+
+            passingTime = 0;
+            foreach (Car b in priorityQueue.Peek())
+            {
+                for (i = 0; i < carNumber; i++)
+                {
+                    if (b.Number == array[i, 0])
+                    {
+                        passingTime += b.ProcessingTime;
+                        array[i, 2] = passingTime;
+                    }
+                }
+            }
+
+            for (i = 0; i < carNumber; i++)
+            {
+                if (array[i, 1] > array[i, 2])
+                {
+                    difference = array[i, 1] - array[i, 2];
+                    percentage = ((array[i, 1] - array[i, 2]) / array[i, 2]) * 100;
+                    percentage = Math.Round(percentage, 2);
+                    strDifferencePercentage += "" + " Vehicle number" + array[i, 0] + " --> Waited " + difference + " seconds less. There is " + percentage + " % gain from time." + Environment.NewLine;
+                }
+            }
+            if (strDifferencePercentage == "")
+            {
+                strDifferencePercentage = "No time-saving vehicles.";
+            }
+            return strDifferencePercentage;
+        }
+    }
+}
